6fb69761df1927e9126a65e7f5d04e60b9a50de1
Aditya <adityasaxena602@gmail.com>
2020-11-13 19:14:11 +0530

    add top toolbar

--- src/components/FrontSection.js
+++ src/components/FrontSection.js
@@ -8,7 +8,23 @@ export default function FrontSection() {
     return (
         <div className="frontpage">
             <div className="frontpage__header">
-
+                <div className="header">
+                    <div className="options__left">
+                        <ul className="options__list">
+                            <li>projects</li>
+                            <li>resume</li>
+                        </ul>
+                    </div>
+                    <div className="brand__logo">
+                        <img src="./letter-a.png" alt=""/>
+                    </div>
+                    <div className="options__right">
+                        <ul className="options__list">
+                            <li>blog</li>
+                            <li>journey</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div className="frontpage__greetings">
                 <div className="greetings">
@@ -19,7 +35,12 @@ export default function FrontSection() {
                         
                         <p className="hello">Hello, I'm</p>
                         <p className="name">Aditya Saxena</p>
-                        <p className="dsg">and I'm a <span className="post">Software Engineer</span></p>
+                        <p className="dsg">and I'm a 
+                            <span className="post">
+                                <span className="green">Soft</span><span className="blue">ware</span> 
+                                <span className="orange">Engi</span><span className="red">neer</span>
+                            </span>
+                        </p>
                         <p className="company"> @ Samsung Research Institute</p>
                         <div className="bg__icon"><FontAwesomeIcon icon={faInfo} /></div>
                     </div>
